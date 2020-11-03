ed600e1ddf0c31a8a241889df007cc39158efe4b
Stephany Rivera <anie.12106@gmail.com>
2020-11-03 08:27:06 -0500

    control tanque faltan algunas caracteristicas

---
+++ ControlTanque/Scripts/MinusPlus.js
@@ -0,0 +1,152 @@
+﻿//plugin bootstrap minus and plus
+//http://jsfiddle.net/laelitenetwork/puJ6G/
+
+
+
+$('.btn-number').click(function(e){
+    e.preventDefault();
+    
+    fieldName = $(this).attr('data-field');
+    type      = $(this).attr('data-type');
+    var input = $("input[name='"+fieldName+"']");
+    var currentVal = parseInt(input.val());
+    if (!isNaN(currentVal)) {
+        if(type == 'minus') {
+            
+            if(currentVal > input.attr('min')) {
+                input.val(currentVal - 1).change();
+                if (fieldName == 'modify') {                   
+                    tank(parseInt(input.val()));                   
+                }
+                if (name == 'maximum' || name == 'minimum') {
+                    updateMaximumMinimum();
+                }
+            } 
+            if(parseInt(input.val()) == input.attr('min')) {
+                $(this).attr('disabled', true);
+            }
+
+        } else if(type == 'plus') {
+
+            if(currentVal < input.attr('max')) {
+                input.val(currentVal + 1).change();
+                if (fieldName == 'modify') {
+                    tank(parseInt(input.val()));                    
+                }
+                if (name == 'maximum' || name == 'minimum') {
+                    updateMaximumMinimum();
+                }
+            }
+            if(parseInt(input.val()) == input.attr('max')) {
+                $(this).attr('disabled', true);
+            }
+
+        }
+    } else {
+        input.val(0);
+    }
+});
+
+
+
+$('.input-number').focusin(function(){
+    $(this).data('oldValue', $(this).val());       
+});
+$('.input-number').change(function () {    
+    minValue =  parseInt($(this).attr('min'));
+    maxValue =  parseInt($(this).attr('max'));
+    valueCurrent = parseInt($(this).val());    
+    
+    name = $(this).attr('name');
+    if(valueCurrent >= minValue) {
+        $(".btn-number[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
+        if (name == 'modify') {
+            tank(valueCurrent);            
+        }
+        if(name=='maximum' || name=='minimum'){
+            updateMaximumMinimum();
+        }
+        
+    } else {
+        alert('Lo siento, Este valor supera la cantidad mínimima permitida');
+        $(this).val($(this).data('oldValue'));        
+        if (name == 'modify') {
+            tank(val($(this).data('oldValue')));
+        }
+    }
+    if(valueCurrent <= maxValue) {
+        $(".btn-number[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+        if (name == 'modify') {
+            tank(valueCurrent);
+        }
+        if (name == 'maximum' || name == 'minimum') {
+            updateMaximumMinimum();
+        }
+    } else {
+        alert('Lo siento, Este valor supera la cantidad máxima permitida');
+        $(this).val($(this).data('oldValue'));       
+        if (name == 'modify') {
+            tank(val($(this).data('oldValue')));
+        }
+    }
+    
+    
+});
+$(".input-number").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+             // Allow: Ctrl+A
+            (e.keyCode == 65 && e.ctrlKey === true) || 
+             // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+});
+
+
+
+
+//enabled-disabled 
+
+var inputMax = document.getElementById("inputMaximum");
+var inputMin = document.getElementById("inputMinimum");
+var inputMinusMax = document.getElementById("minusMax");
+var inputPlusMax = document.getElementById("plusMax");
+var inputMinusMin = document.getElementById("minusMin");
+var inputPlusMin = document.getElementById("plusMin");
+
+inputMax.addEventListener("focus", focus, true);
+inputMax.addEventListener("blur", blur, true);
+inputMin.addEventListener("focus", focus, true);
+inputMin.addEventListener("blur", blur, true);
+
+inputMinusMax.addEventListener("focus", focus, true);
+inputMinusMax.addEventListener("blur", blur, true);
+inputPlusMax.addEventListener("focus", focus, true);
+inputPlusMax.addEventListener("blur", blur, true);
+
+inputMinusMin.addEventListener("focus", focus, true);
+inputMinusMin.addEventListener("blur", blur, true);
+inputPlusMin.addEventListener("focus", focus, true);
+inputPlusMin.addEventListener("blur", blur, true);
+
+
+function focus() {
+    document.getElementById("inputModify").disabled = true;
+    document.getElementById("minusModify").disabled = true;
+    document.getElementById("plusModify").disabled = true;
+    
+}
+
+function blur() {
+    document.getElementById("inputModify").disabled = false;
+    document.getElementById("minusModify").disabled = false;
+    document.getElementById("plusModify").disabled = false;
+   
+}
+
---
+++ ControlTanque/Scripts/fill.js
@@ -0,0 +1,169 @@
+﻿
+///Init (initial program values)   
+var maximum, minimum, highlevel, lowlevel, valueTank;
+var maximumCapacity;
+    maximum = 100;
+    minimum = 1;
+    highlevel = 84;
+    lowlevel = 15;
+    maximumCapacity = 100000;
+    
+    
+    
+
+//Init elements
+    document.getElementById("levelShow").value = 35 ;
+    document.getElementById("inputModify").value = 35;
+    document.getElementById("inputMaximum").value = maximum ;
+    document.getElementById("inputMinimum").value = minimum ;
+    document.getElementById("inputHigh").value = highlevel;
+    document.getElementById("inputLow").value = lowlevel;
+
+//Init maximum value fields for capacity  
+    document.getElementById("inputMaximum").min = 0;
+    document.getElementById("inputMaximum").max = maximumCapacity;    
+    document.getElementById("inputMinimum").min = 0;
+    document.getElementById("inputMinimum").max = maximumCapacity;
+    
+    
+    
+    valueTank = 35;    
+    tank(valueTank);
+       
+
+///Tank
+
+
+    var _hmt = _hmt || [];
+    (function () {
+        var hm = document.createElement('script');
+        hm.src = '//hm.baidu.com/hm.js?4bad1df23f079e0d12bdbef5e65b072f';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+  
+    
+
+    function tank(level) {       
+        var percentage = ((level * 100) / (maximum - minimum)) / 100;
+        var options = {
+            series: [{
+                type: 'liquidFill',
+                waveAnimation: true,
+                animation: true,
+                data: [{
+                    value: percentage
+                }],
+                shape: 'container',
+            }]
+        };
+
+        var chart = echarts.init(document.getElementById('chart'));
+        chart.setOption(options);
+        window.onresize = function () {
+            chart.resize();            
+        };
+
+        document.getElementById("levelShow").value = level;
+
+
+    }
+
+
+
+
+///Updates
+
+        function update(levelCurrent) {
+            document.getElementById("levelShow").value = levelCurrent;
+
+        }
+        
+        function updateMaximumMinimum() {
+            var newMinimum = document.getElementById("inputMinimum").value;
+            var newMaximum = document.getElementById("inputMaximum").value;
+            maximum = newMaximum;
+            minimum = newMinimum;
+
+            var currentValueModify = document.getElementById("inputModify").value;
+            var currenValueAlertH = document.getElementById("inputHigh").value;
+            var currentValueAlertL = document.getElementById("inputLow").value;
+            
+            if (currentValueModify > newMaximum) {
+                document.getElementById("inputModify").value = newMaximum;
+            }
+            else if (currentValueModify < newMinimum) {
+                document.getElementById("inputModify").value = newMinimum;
+            }
+
+            if (currentValueAlertL > newMaximum) {
+                document.getElementById("inputLow").value = newMaximum;
+            }
+            else if (currentValueAlertL < newMinimum) {
+                document.getElementById("inputLow").value = newMinimum;
+            }
+            
+            if (currenValueAlertH > newMaximum) {
+                document.getElementById("inputHigh").value = newMaximum;
+            }
+            else if (currenValueAlertH < newMinimum) {
+                document.getElementById("inputHigh").value = newMinimum;
+            }
+
+
+            document.getElementById("inputModify").min = newMinimum;
+            document.getElementById("inputModify").max = newMaximum;
+            document.getElementById("inputLow").min = newMinimum;
+            document.getElementById("inputLow").max=newMaximum;
+            document.getElementById("inputHigh").max = newMaximum;
+            document.getElementById("inputLow").min = newMinimum;
+
+            tank(document.getElementById("inputModify").value);
+
+        }
+
+//Clear tank minimum capacity
+
+
+        document.getElementById("inputClear").onclick = function () { clearTank() };
+
+        function clearTank() {
+            if (confirm("Estás seguro que deseas vaciar el tanque")) {
+                var minimum = document.getElementById("inputMinimum").value;
+                tank(minimum);
+                document.getElementById("levelShow").value = minimum;
+                document.getElementById("inputModify").value = minimum;                
+            } 
+            
+        }
+
+
+// Alarm
+        setInterval(alarm(), 3000);
+        function alarm() {
+            console.log("alarma");
+            var highLevel=document.getElementById("inputHigh").value;
+            var lowLevel = document.getElementById("inputLow").value;
+            var currentLevel = document.getElementById("levelShow").value;
+            if (currentLevel>=highlevel) {
+                window.alert("El tanque tiene niveles altos");
+            }
+            if (currentLevel <= lowlevel) {
+
+                window.alert("El tanque tiene niveles bajos");
+            }
+
+            
+        
+        
+        }
+
+
+    
+
+
+        
+
+
+ 
+
